24617d51840e337d9364421cddc9a29fc643275f
Mizpuno <nititorn.certihunt@gmail.com>
2023-07-29 13:46:49 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title: string 
+
+  constructor() {
+    this.title = 'hello-angular'
+  }
 }
+
